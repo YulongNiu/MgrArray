c8598201c262469aa6d9e5581a356febfa20ea51
Yulong Niu <niuylscu@gmail.com>
2016-12-30 15:24:49 +0800

    add proid and uniid

--- script_KEGGID.R
+++ script_KEGGID.R
@@ -34,13 +34,36 @@ rownames(mRNAMat) <- NULL
 colnames(mRNAMat) <- c('geneid', 'mRNAid')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-##~~~~~~~~~~~~~~~~~~~~KEGG ID to NCBI gene ID~~~~~~~~~~~~~~~~~~~~~~~~
+##~~~~~~~~~~~~~~~~~~~~KEGG ID to NCBI gene ID~~~~~~~~~~~~~~~~~~~~~
 KEGGIDMat <- convKEGG('ncbi-geneid', 'mgr')
 KEGGIDMat[, 2] <- sapply(strsplit(KEGGIDMat[, 2], split = ':', fixed = TRUE), '[[', 2)
 colnames(KEGGIDMat) <- c('KEGGID', 'geneid')
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-## merge IDs
+##~~~~~~~~~~~~~~~~~~~~~~~merge IDs with array probes~~~~~~~~~~~~~~~~~~
+load('/home/Yulong/RESEARCH/BaiLinhan_array/Process/mgrArray.RData')
+
 mergeAnno <- merge(KEGGIDMat, mRNAMat, by.x = 'geneid', by.y = 'geneid')
-save(mergeAnno, file = 'mergeAnno.RData')
+
+## with array probes
+arrayAnno <- mgrArray$genes[, c('ProbeName', 'RefSeqAccession')]
+mergeGeneAnno <- merge(mergeAnno, arrayAnno, by.x = 'mRNAid', by.y = 'RefSeqAccession')
+save(mergeGeneAnno, file = 'mergeGeneAnno.RData')
+
+
+## with ncbi-proteinid and uniprot
+proidMat <- convKEGG('ncbi-proteinid', 'mgr')
+proidMat[, 2] <- sapply(strsplit(proidMat[, 2], split = ':', fixed = TRUE), '[[', 2)
+colnames(proidMat) <- c('KEGGID', 'proteinid')
+
+unidMat <- convKEGG('uniprot', 'mgr')
+unidMat[, 2] <- sapply(strsplit(unidMat[, 2], split = ':', fixed = TRUE), '[[', 2)
+colnames(unidMat) <- c('KEGGID', 'uniprotid')
+
+mergeProUniMat <- merge(proidMat, unidMat, by.x = 'KEGGID', by.y = 'KEGGID')
+mergeProAnno <- merge(mergeGeneAnno, mergeProUniMat, by.x = 'KEGGID', by.y = 'KEGGID')
+save(mergeProAnno, file = 'mergeProAnno.RData')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ######################################################################
+
+
