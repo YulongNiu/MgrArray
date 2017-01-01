2767fd0f966b10e29bccc3dabc9d1be8ef8b40e3
Yulong Niu <niuylscu@gmail.com>
2017-01-01 14:19:34 +0800

    add GO analysis

--- script_GO.R
+++ script_GO.R
@@ -1,4 +1,6 @@
 ########################convert GO##################################
+setwd('/home/Yulong/RESEARCH/BaiLinhan_array/Process/')
+
 library('clusterProfiler')
 library('stringr')
 library('GO.db')
@@ -6,7 +8,6 @@ library('GO.db')
 load('mergeProAnno.RData')
 mergeProAnno[, 1] <- sapply(strsplit(as.character(mergeProAnno[, 1]), split = ':', fixed = TRUE), '[[', 2)
 DEGMat <- read.csv('DEGMat.csv', stringsAsFactor = FALSE, row.names = 1)
-load('mergeGeneAnno.RData')
 
 rawGO <- read.csv('gene_association.PAMGO_Mgrisea', sep = '\t', skip = 24, stringsAsFactor = FALSE, header = FALSE)
 rawGO <- rawGO[, c(11, 5, 9)]
@@ -35,9 +36,76 @@ thres <- 0.05
 DEGProbes <- rownames(DEGMat)[DEGMat[, 'P.Value'] < 0.05]
 DEGIDs <- mergeProAnno[mergeProAnno[, 'ProbeName'] %in% DEGProbes, 'KEGGID']
 
-## BP
+##~~~~~~~~~~~~~~~~~~~~~~~~~BP~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 proGOBP <- proGO[proGO[, 3] == 'P', ]
 bp <- enricher(DEGIDs, pvalueCutoff = 1, universe = unique(proGO[, 1]), minGSSize = 5, qvalueCutoff = 1, TERM2GENE = data.frame(proGOBP[, 2:1]), TERM2NAME = data.frame(proGOBP[, c(2, 4)]))
+
+showNum <- 10
+pdf('BPbar.pdf', width = 10)
+barplot(bp, showCategory = showNum)
+dev.off()
+pdf('BPdot.pdf', width = 10)
+dotplot(bp, showCategory = showNum)
+dev.off()
+
+## convert to probe IDs
+bp <- as.data.frame(bp)
+bp[, 8] <- sapply(bp[, 8], function(x) {
+  splitVec <- unlist(strsplit(x, split = '/', fixed = TRUE))
+  convVec <- mergeProAnno[mergeProAnno[, 'KEGGID'] %in% splitVec, 'ProbeName']
+  convVec <- paste(convVec, collapse = '/')
+  return(convVec)
+})
+
 write.csv(bp, file = 'BPenrich.csv')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~MF~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+proGOMF <- proGO[proGO[, 3] == 'F', ]
+mf <- enricher(DEGIDs, pvalueCutoff = 1, universe = unique(proGO[, 1]), minGSSize = 5, qvalueCutoff = 1, TERM2GENE = data.frame(proGOMF[, 2:1]), TERM2NAME = data.frame(proGOMF[, c(2, 4)]))
+
+showNum <- 6
+pdf('MFbar.pdf', width = 15)
+barplot(mf, showCategory = showNum)
+dev.off()
+pdf('MFdot.pdf', width = 15)
+dotplot(mf, showCategory = showNum)
+dev.off()
+
+## convert to probe IDs
+mf <- as.data.frame(mf)
+mf[, 8] <- sapply(mf[, 8], function(x) {
+  splitVec <- unlist(strsplit(x, split = '/', fixed = TRUE))
+  convVec <- mergeProAnno[mergeProAnno[, 'KEGGID'] %in% splitVec, 'ProbeName']
+  convVec <- paste(convVec, collapse = '/')
+  return(convVec)
+})
+
+write.csv(mf, file = 'MFenrich.csv')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+##~~~~~~~~~~~~~~~~~~~~~~~~~CC~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+proGOCC <- proGO[proGO[, 3] == 'C', ]
+cc <- enricher(DEGIDs, pvalueCutoff = 1, universe = unique(proGO[, 1]), minGSSize = 5, qvalueCutoff = 1, TERM2GENE = data.frame(proGOCC[, 2:1]), TERM2NAME = data.frame(proGOCC[, c(2, 4)]))
+
+showNum <- 6
+pdf('CCbar.pdf', width = 10)
+barplot(cc, showCategory = showNum)
+dev.off()
+pdf('CCdot.pdf', width = 10)
+dotplot(cc, showCategory = showNum)
+dev.off()
+
+## convert to probe IDs
+cc <- as.data.frame(cc)
+cc[, 8] <- sapply(cc[, 8], function(x) {
+  splitVec <- unlist(strsplit(x, split = '/', fixed = TRUE))
+  convVec <- mergeProAnno[mergeProAnno[, 'KEGGID'] %in% splitVec, 'ProbeName']
+  convVec <- paste(convVec, collapse = '/')
+  return(convVec)
+})
+
+write.csv(cc, file = 'CCenrich.csv')
+##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 ####################################################################
